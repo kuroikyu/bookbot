4a3ce5438f7ab56204f817916eae78369af6d5f4
Kuroi Kyu <hello@kuroikyu.com>
2024-11-02 07:45:30 +0100

    count how many times each character is in the book

--- main.py
+++ main.py
@@ -6,5 +6,21 @@ def main():
         words = file_contents.split()
 
         print(len(words))
+        count_characters(file_contents)
+
+
+def count_characters(text):
+    lowered_text = text.lower()
+    ## print(lowered_text)
+
+    result = {}
+
+    for char in lowered_text:
+        if char in result:
+            result[char] += 1
+        else:
+            result[char] = 1
+    
+    print(result)
 
 main()
