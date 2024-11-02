87b2299c4e51edca7868a453ada7c8edb6439fb2
Kuroi Kyu <hello@kuroikyu.com>
2024-11-02 08:37:53 +0100

    format and print a report

--- main.py
+++ main.py
@@ -1,18 +1,30 @@
 def main():
-    with open("books/frankenstein.txt") as f:
+    book_name = "books/frankenstein.txt"
+    with open(book_name) as f:
         file_contents = f.read()
+
+        print(f"--- Begin report of {book_name} ---")
 
         # Print number of words in the file
         words = file_contents.split()
 
-        print(len(words))
-        count_characters(file_contents)
+        print(f"{len(words)} words found in the document \n")
+
+        book_char_count = count_characters(file_contents)
+        book_filtered_char_count = filter_dict(book_char_count)
+        book_sorted_char_count = sort_dict_to_list(book_filtered_char_count)
+
+        #print(book_sorted_char_count)
+        for dict in book_sorted_char_count:
+            # print(dict["char"])
+            print(f"The \'{dict['char']}\' character was found {dict['num']} times")
+
+        print("--- End report ---")
+
 
 
 def count_characters(text):
     lowered_text = text.lower()
-    ## print(lowered_text)
-
     result = {}
 
     for char in lowered_text:
@@ -20,7 +32,26 @@ def count_characters(text):
             result[char] += 1
         else:
             result[char] = 1
-    
-    print(result)
+    return result
+
+def filter_dict(dict):
+    filtered_dict = {}
+    for elem, val in dict.items():
+        if elem.isalpha():
+            filtered_dict[elem] = val
+
+    return filtered_dict
+
+def sort_on(dict):
+    return dict["num"]
+
+def sort_dict_to_list(dict):
+    dict_list = []
+
+    for elem, val in dict.items():
+        dict_list.append({ "char": elem, "num": val  })
+    dict_list.sort(reverse=True, key=sort_on)
+
+    return dict_list
 
 main()
